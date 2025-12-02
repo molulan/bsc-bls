1bcb36a54b3b0430411f14a381d7e0ef96fe88da
molulan <github.grumpily681@passmail.net>
2025-12-02 15:02:44 +0100

    Implement aggregate multisignature protocol and initial positive test

--- bls.go
+++ bls.go
@@ -52,3 +52,87 @@ func hashToG1(msg []byte) *e.G1 {
 
 	return h
 }
+
+func KeyAggregation(pks []PublicKey) PublicKey {
+	apk := new(e.G2)
+	apk.SetIdentity()
+
+	for _, pk := range pks {
+		a := hashToScalar(pk, pks)
+
+		temp := new(e.G2)
+		temp.ScalarMult(a, pk)
+
+		apk.Add(apk, temp)
+	}
+
+	return apk
+}
+
+func hashToScalar(pk PublicKey, pks []PublicKey) *e.Scalar {
+	pkSerialized := (*pk).Bytes()
+
+	pksSerialized := make([]byte, 0)
+	for _, pk := range pks {
+		pksSerialized = append(pksSerialized, (*pk).Bytes()...)
+	}
+
+	combined := append(pksSerialized, pkSerialized...)
+	hash := sha256.Sum256(combined)
+
+	scalar := new(e.Scalar)
+	scalar.SetBytes(hash[:])
+
+	return scalar
+}
+
+func MuSign(msg Message, kp KeyPair, pks []PublicKey, apk PublicKey) Signature {
+	msg = append(msg, (*apk).Bytes()...)
+
+	a := hashToScalar(kp.PublicKey, pks)
+
+	temp := new(e.Scalar)
+	temp.Mul(a, kp.SecretKey)
+
+	partialSigature := new(e.G1)
+	partialSigature.ScalarMult(temp, hashToG1(msg))
+
+	return partialSigature
+}
+
+func SignatureAggregation(sigs []Signature) Signature {
+	aggregateSignature := new(e.G1)
+	aggregateSignature.SetIdentity()
+
+	for _, sig := range sigs {
+		aggregateSignature.Add(aggregateSignature, sig)
+	}
+
+	return aggregateSignature
+}
+
+func VerifyMultisig(msg Message, sig Signature, apk PublicKey) bool {
+	msg = append(msg, (*apk).Bytes()...)
+	h := hashToG1(msg)
+
+	gt1 := e.Pair(h, apk)
+
+	gt2 := e.Pair(sig, e.G2Generator())
+
+	return gt1.IsEqual(gt2)
+}
+
+func VerifyAggregateMultisig(msgs []Message, sig Signature, apks []PublicKey) bool {
+	left := e.Pair(sig, e.G2Generator())
+
+	right := new(e.Gt)
+	right.SetIdentity()
+
+	for i, msg := range msgs {
+		msg = append(msg, (*apks[i]).Bytes()...)
+		next := e.Pair(hashToG1(msg), apks[i])
+		right.Mul(right, next)
+	}
+
+	return left.IsEqual(right)
+}
--- bls_test.go
+++ bls_test.go
@@ -62,3 +62,110 @@ func TestKeyGenReturnsUniqueKeyPairs(t *testing.T) {
 		publicKeySet[kp.PublicKey] = true
 	}
 }
+
+func TestMuSignAndMuVerifyWith1Signer(t *testing.T) {
+	msg := []byte("msg")
+
+	keyPair := KeyGen()
+
+	pks := []PublicKey{keyPair.PublicKey}
+
+	apk := KeyAggregation(pks)
+
+	signature := MuSign(msg, keyPair, pks, apk)
+
+	ok := VerifyMultisig(msg, signature, apk)
+
+	if !ok {
+		t.Errorf("Signature should be valid")
+	}
+}
+
+func TestMuSignAndMuVerifyWithMultipleSigners(t *testing.T) {
+	n := 100
+
+	msg := []byte("msg")
+
+	pks := make([]PublicKey, n)
+	kps := make([]KeyPair, n)
+
+	for i := range n {
+		kp := KeyGen()
+		kps[i] = kp
+		pks[i] = kp.PublicKey
+	}
+
+	apk := KeyAggregation(pks)
+
+	partial_sigs := make([]Signature, n)
+
+	for i, kp := range kps {
+		partial_sigs[i] = MuSign(msg, kp, pks, apk)
+	}
+
+	multiSig := SignatureAggregation(partial_sigs)
+
+	ok := VerifyMultisig(msg, multiSig, apk)
+
+	if !ok {
+		t.Errorf("Signature should be valid")
+	}
+}
+
+func TestMuSignAndMuVerifyWithAggregatedMultiSig(t *testing.T) {
+
+	msg1 := []byte("msg1")
+	msg2 := []byte("msg2")
+	msg3 := []byte("msg3")
+
+	kps1, pks1, apk1 := setupKeys(5)
+	kps2, pks2, apk2 := setupKeys(3)
+	kps3, pks3, apk3 := setupKeys(4)
+
+	partial_sigs1 := make([]Signature, 5)
+	partial_sigs2 := make([]Signature, 3)
+	partial_sigs3 := make([]Signature, 4)
+
+	for i, kp := range kps1 {
+		partial_sigs1[i] = MuSign(msg1, kp, pks1, apk1)
+	}
+	for i, kp := range kps2 {
+		partial_sigs2[i] = MuSign(msg2, kp, pks2, apk2)
+	}
+	for i, kp := range kps3 {
+		partial_sigs3[i] = MuSign(msg3, kp, pks3, apk3)
+	}
+
+	multiSig1 := SignatureAggregation(partial_sigs1)
+	multiSig2 := SignatureAggregation(partial_sigs2)
+	multiSig3 := SignatureAggregation(partial_sigs3)
+
+	multisigs := []Signature{multiSig1, multiSig2, multiSig3}
+
+	aggregateMultiSig := SignatureAggregation(multisigs)
+
+	msgs :=[]Message{msg1, msg2, msg3}
+
+	apks := []PublicKey{apk1, apk2, apk3}
+
+	ok := VerifyAggregateMultisig(msgs, aggregateMultiSig, apks)
+
+	if !ok {
+		t.Errorf("Signature should be valid")
+	}
+}
+
+func setupKeys(n int) ([]KeyPair, []PublicKey, PublicKey) {
+	pks := make([]PublicKey, n)
+	kps := make([]KeyPair, n)
+
+	for i := range n {
+		kp := KeyGen()
+		kps[i] = kp
+		pks[i] = kp.PublicKey
+	}
+
+	apk := KeyAggregation(pks)
+
+	return kps, pks, apk
+}
