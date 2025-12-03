ca122b7b7c23ce7151bee2d27057606cd23d7af0
molulan <github.grumpily681@passmail.net>
2025-12-03 18:31:35 +0100

    restructure tests into multiple testfiles for better readability

---
+++ crypto/keygen_test.go
@@ -0,0 +1,20 @@
+package crypto
+
+import (
+	"testing"
+)
+
+func TestKeyGenReturnsUniqueKeyPairs(t *testing.T) {
+	secretKeySet := make(map[SecretKey]bool)
+	publicKeySet := make(map[PublicKey]bool)
+
+	for range 1000 {
+		kp := KeyGen()
+		if secretKeySet[kp.SecretKey] || publicKeySet[kp.PublicKey] {
+			t.Error("duplicate key found")
+		}
+
+		secretKeySet[kp.SecretKey] = true
+		publicKeySet[kp.PublicKey] = true
+	}
+}
