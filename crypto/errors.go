d4c2cda5f2a2b9e3d1602ce0b5a41e78061a2174
molulan <github.grumpily681@passmail.net>
2025-12-03 11:59:42 +0100

    refactor logic for multisig context and add errors

---
+++ crypto/errors.go
@@ -0,0 +1,12 @@
+package crypto
+
+import (
+	"errors"
+)
+
+var (
+    ErrNoPublicKeys 			= errors.New("at least one public key is required")
+	ErrNoSignatures				= errors.New("at least one signature is required")
+	ErrNoMessages				= errors.New("at least one message is required")
+	ErrMismatchedLengths		= errors.New("mismatched slice lengths")
+)
--- crypto/multisig.go
+++ crypto/multisig.go
@@ -5,7 +5,52 @@ import (
 )
 
 
-func KeyAggregation(pks []PublicKey) PublicKey {
+func NewMultiSigContext(participants []PublicKey) (*MultisigContext, error) {
+	aggregatePk, err := KeyAggregation(participants)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MultisigContext{
+		Participants: participants,
+		AggregatePk: aggregatePk,
+	}, nil
+}
+
+
+func (ctx *MultisigContext) Sign(msg Message, kp KeyPair) Signature {
+	
+	msg = append(msg, (*ctx.AggregatePk).Bytes()...)
+
+	a := hashToScalar(kp.PublicKey, ctx.Participants)
+
+	temp := new(e.Scalar)
+	temp.Mul(a, kp.SecretKey)
+
+	partialMultisig := new(e.G1)
+	partialMultisig.ScalarMult(temp, hashToG1(msg))
+
+	return partialMultisig
+}
+
+
+func (ctx *MultisigContext) Verify(msg Message, sig Signature) bool {
+	msg = append(msg, (*ctx.AggregatePk).Bytes()...)
+	h := hashToG1(msg)
+
+	gt1 := e.Pair(h, ctx.AggregatePk)
+
+	gt2 := e.Pair(sig, e.G2Generator())
+
+	return gt1.IsEqual(gt2)
+}
+
+
+func KeyAggregation(pks []PublicKey) (PublicKey, error) {
+	if len(pks) == 0 {
+		return nil, ErrNoPublicKeys
+	}
+
 	apk := new(e.G2)
 	apk.SetIdentity()
 
@@ -18,26 +63,15 @@ func KeyAggregation(pks []PublicKey) PublicKey {
 		apk.Add(apk, temp)
 	}
 
-	return apk
+	return apk, nil
 }
 
 
-func MuSign(msg Message, kp KeyPair, pks []PublicKey, apk PublicKey) Signature {
-	msg = append(msg, (*apk).Bytes()...)
+func SignatureAggregation(sigs []Signature) (Signature, error) {
+	if len(sigs) == 0 {
+		return nil, ErrNoSignatures
+	}
 
-	a := hashToScalar(kp.PublicKey, pks)
-
-	temp := new(e.Scalar)
-	temp.Mul(a, kp.SecretKey)
-
-	partialSigature := new(e.G1)
-	partialSigature.ScalarMult(temp, hashToG1(msg))
-
-	return partialSigature
-}
-
-
-func SignatureAggregation(sigs []Signature) Signature {
 	aggregateSignature := new(e.G1)
 	aggregateSignature.SetIdentity()
 
@@ -45,33 +79,29 @@ func SignatureAggregation(sigs []Signature) Signature {
 		aggregateSignature.Add(aggregateSignature, sig)
 	}
 
-	return aggregateSignature
+	return aggregateSignature, nil
 }
 
 
-func VerifyMultisig(msg Message, sig Signature, apk PublicKey) bool {
-	msg = append(msg, (*apk).Bytes()...)
-	h := hashToG1(msg)
+func VerifyAggregateMultisig(msgs []Message, sig Signature, apks []PublicKey) (bool, error) {
+	if len(msgs) == 0 {
+		return false, ErrNoMessages
+	}
 
-	gt1 := e.Pair(h, apk)
+	if len(msgs) != len(apks) {
+		return  false, ErrMismatchedLengths
+	}
 
-	gt2 := e.Pair(sig, e.G2Generator())
+	gt1 := e.Pair(sig, e.G2Generator())
 
-	return gt1.IsEqual(gt2)
-}
-
-
-func VerifyAggregateMultisig(msgs []Message, sig Signature, apks []PublicKey) bool {
-	left := e.Pair(sig, e.G2Generator())
-
-	right := new(e.Gt)
-	right.SetIdentity()
+	gt2 := new(e.Gt)
+	gt2.SetIdentity()
 
 	for i, msg := range msgs {
 		msg = append(msg, (*apks[i]).Bytes()...)
 		next := e.Pair(hashToG1(msg), apks[i])
-		right.Mul(right, next)
+		gt2.Mul(gt2, next)
 	}
 
-	return left.IsEqual(right)
+	return gt1.IsEqual(gt2), nil
 }
